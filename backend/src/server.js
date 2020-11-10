fecfa7109e430b0547455972f7e849f905161272
MartinGK <mrtgeka@gmail.com>
2020-11-10 02:25:05 -0300

    test heroku deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,10 +11,10 @@ app.use(cors())
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (request, response) => {
-        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        response.sendFile(path.join(__dirname + '../../frontend/build/index.html'))
     });
 }
 
